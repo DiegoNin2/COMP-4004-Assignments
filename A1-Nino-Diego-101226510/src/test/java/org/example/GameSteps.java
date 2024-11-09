5c612619a04820251b0ceea8ebbf46074e32b965
DiegoNin2 <99743877+DiegoNin2@users.noreply.github.com>
2024-11-09 14:43:05 -0500

    2winner_game_2winner_quest Scenario
    code for the 2nd scenario
    bit of a refactor also in an attempt to make the step definitions less hard coded (i'm not sure if it worked well)

--- A1-Nino-Diego-101226510/src/test/java/org/example/GameSteps.java
+++ A1-Nino-Diego-101226510/src/test/java/org/example/GameSteps.java
@@ -10,8 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameSteps {
 
     private Main game;
+    StringWriter output = new StringWriter();
 
-    //ToDO: see if i can make this reusable later
+    //game starting steps
+    //TODO: see if i can make this reusable later
     @Given("a new game starts with A1_Scenario")
     public void a_new_game_starts_A1_Scenario() {
         //i literally just ripped the code from the MainAcceptanceTest
@@ -101,22 +103,92 @@ public class GameSteps {
         game.adventureDeck.set(9,c10);
     }
 
-    //TODO: see if i can make this reusable later
-    @When("A 4 stage quest card")
-    public void quest_card_drawn() {
-        //rigging the event deck so that the right stage quest card is drawn
-        for (int i = 0; i < game.getEventDeckSize(); i++) {
-            if (game.eventDeck.get(i).getName().equals("4 Stage Quest")) {
-                game.eventDeck.set(0, game.eventDeck.get(i));
-            }
+    @Given("a new game starts with 2winner_game")
+    public void a_new_game_starts_2winner_game() {
+        game = new Main();
+        game.initializeDecks();
+        game.initializePlayers();
+        game.dealCards();
+
+        //card rigging
+        for (int i = 0; i < game.playerList.size(); i++) {
+            game.playerList.get(i).getHand().clear();
+        }
+
+        //P1 Hand
+        game.pickCard(0, "Foe", "F10", "10", 1);
+        game.pickCard(1, "Foe", "F15", "15", 1);
+        game.pickCard(2, "Foe", "F20", "20", 1);
+        game.pickCard(3, "Foe", "F25", "25", 1);
+        game.pickCard(4, "Foe", "F5", "5", 1);
+        game.pickCard(5, "Foe", "F5", "5", 1);
+        game.pickCard(6, "Weapon", "Lance", "20", 1);
+        game.pickCard(7, "Weapon", "Lance", "20", 1);
+        game.pickCard(8, "Weapon", "Lance", "20", 1);
+        game.pickCard(9, "Weapon", "Lance", "20", 1);
+        game.pickCard(10, "Foe", "F50", "50", 1);
+        game.pickCard(11, "Foe", "F50", "50", 1);
+
+        //P2 Hand
+        game.pickCard(0, "Weapon", "Sword", "10", 2);
+        game.pickCard(1, "Weapon", "Battle-Axe", "15", 2);
+        game.pickCard(2, "Weapon", "Horse", "10", 2);
+        game.pickCard(3, "Weapon", "Lance", "20", 2);
+        game.pickCard(4, "Weapon", "Lance", "20", 2);
+        game.pickCard(5, "Weapon", "Dagger", "5", 2);
+        game.pickCard(6, "Weapon", "Dagger", "5", 2);
+        game.pickCard(7, "Weapon", "Sword", "10", 2);
+        game.pickCard(8, "Weapon", "Horse", "10", 2);
+        game.pickCard(9, "Weapon", "Dagger", "5", 2);
+        game.pickCard(10, "Foe", "F10", "10", 2);
+        game.pickCard(11, "Foe", "F5", "5", 2);
+
+        //P3 Hand
+        game.pickCard(0, "Weapon", "Dagger", "5", 3);
+        game.pickCard(1, "Foe", "F5", "5", 3);
+        game.pickCard(2, "Foe", "F10", "10", 3);
+        game.pickCard(3, "Foe", "F15", "15", 3);
+        game.pickCard(4, "Foe", "F70", "70", 3);
+        game.pickCard(5, "Weapon", "Sword", "10", 3);
+        game.pickCard(6, "Weapon", "Sword", "10", 3);
+        game.pickCard(7, "Weapon", "Sword", "10", 3);
+        game.pickCard(8, "Weapon", "Horse", "10", 3);
+        game.pickCard(9, "Weapon", "Horse", "10", 3);
+        game.pickCard(10, "Weapon", "Horse", "10", 3);
+        game.pickCard(11, "Foe", "F20", "20", 3);
+
+        //P4 Hand
+        game.pickCard(0, "Weapon", "Horse", "10", 4);
+        game.pickCard(1, "Weapon", "Battle-Axe", "15", 4);
+        game.pickCard(2, "Weapon", "Horse", "10", 4);
+        game.pickCard(3, "Weapon", "Sword", "10", 4);
+        game.pickCard(4, "Weapon", "Excalibur", "30", 4);
+        game.pickCard(5, "Weapon", "Dagger", "5", 4);
+        game.pickCard(6, "Weapon", "Sword", "10", 4);
+        game.pickCard(7, "Weapon", "Sword", "10", 4);
+        game.pickCard(8, "Weapon", "Battle-Axe", "15", 4);
+        game.pickCard(9, "Foe", "F5", "5", 4);
+        game.pickCard(10, "Foe", "F10", "10", 4);
+        game.pickCard(11, "Foe", "F15", "15", 4);
+
+        //rig adventure deck with F5 foe cards because im lazy and don't want to deal with randomness
+        for (int i = 0; i < 23; i++) {
+            Card c = new Card("Foe" , "F5", "5");
+            game.adventureDeck.set(i,c);
         }
     }
 
-    //TODO: see if i can make this reusable later
-    @And("Player 2 sponsors the quest")
-    public void player_sponsors_quest() {
+    //drawing steps
+    @When("a {string} card is drawn with Player 2 sponsoring")
+    public void single_quest_card_drawn(String questCard) {
         //i literally just ripped the code from the MainAcceptanceTest again
         //not sure why i shouldn't really
+        for (int i = 0; i < game.getEventDeckSize(); i++) {
+            if (game.eventDeck.get(i).getName().equals(questCard)) {
+                game.eventDeck.set(0, game.eventDeck.get(i));
+            }
+        }
+
         String input = "n\ny" + //for sponsoring
                 "\n1\n7\nQuit\n2\n5\nQuit\n2\n3\n4\nQuit\n2\n3\nQuit" + //for stage building
                 "\nparticipate\nparticipate\nparticipate" + //stage 1 participation
@@ -134,8 +206,46 @@ public class GameSteps {
         game.takeTurn(new Scanner(input), new PrintWriter(output));
     }
 
+    @When("a {string} card is drawn with Player 1 sponsoring and a {string} card is drawn with Player 3 sponsoring")
+    public void two_quest_cards_drawn(String questCard1, String questCard2) {
+        //event card rigging here
+        for (int i = 0; i < game.getEventDeckSize(); i++) {
+            if (game.eventDeck.get(i).getName().equals(questCard1)) {
+                game.eventDeck.set(0, game.eventDeck.get(i));
+            } else if (game.eventDeck.get(i).getName().equals(questCard2)) {
+                game.eventDeck.set(1, game.eventDeck.get(i));
+            }
+        }
+
+        //input rigging here
+        String input = "y" + //for sponsoring Q4
+                "\n3\nQuit\n3\nQuit\n3\nQuit\n3\nQuit" + //P1 stage building
+                "\nparticipate\nparticipate\nparticipate" + //stage 1 participation
+                "\n1\n1\n1" + //card trimming
+                "\n6\nquit\n6\nquit\n9\nquit" + //stage 1 attack
+                "\nparticipate\nparticipate" + //stage 2 participation
+                "\n10\n8\nquit\n11\nquit" + //stage 2 attack
+                "\nparticipate\nparticipate" + //stage 3 participation
+                "\n11\nquit\n10\n7\nquit" + //stage 3 attack
+                "\nparticipate\nparticipate" + //stage 4 participation
+                "\n11\n6\nquit\n11\nquit" + //stage 4 attack
+                "\n1\n1\n1\n1\n\n" + //P1 card trimming
+                "\nn\ny" +//for sponsoring Q3
+                "\n1\nQuit\n1\nQuit\n1\nQuit" + //P3 stage building
+                "\nwithdraw\nparticipate\nparticipate" + //stage 1 participation
+                "\n6\nquit\n7\nquit" + //stage 1 attack
+                "\nparticipate\nparticipate" + //stage 2 participation
+                "\n8\nquit\n9\n8\nquit" + //stage 2 attack
+                "\nparticipate\nparticipate" + //stage 3 participation
+                "\n9\n8\nquit\n9\nquit" + //stage 3 attack
+                "\n1\n1\n\n"; //P3 card trimming
+
+        game.takeTurn(new Scanner(input), new PrintWriter(output));
+    }
+
+    //Assert steps
     @Then("Player {int} should have {int} shields")
-    public void p1_should_have_no_shields(int expectedShieldAmount, int playerNum) {
+    public void player_should_have_shields(int expectedShieldAmount, int playerNum) {
         switch (playerNum){
             case 1:
                 assertEquals(expectedShieldAmount, game.playerList.get(0).getShields());
@@ -153,7 +263,7 @@ public class GameSteps {
     }
 
     @And("Player {int} should have {string} in their hand")
-    public void p1_should_have_cards_in_hand(int playerNum, String expectedHand) {
+    public void player_should_have_cards_in_hand(int playerNum, String expectedHand) {
         switch (playerNum) {
             case 1:
                 String p1Hand = game.playerList.get(0).displayHand().replace("\n", ""); //forced to do this because cucumber won't take \n
@@ -177,7 +287,7 @@ public class GameSteps {
     }
 
     @And("Player {int} should have {int} cards in hand")
-    public void p2_should_have_num_of_cards_in_hand(int expectedCardAmount, int playerNum) {
+    public void player_should_have_num_of_cards_in_hand(int expectedCardAmount, int playerNum) {
         switch (playerNum) {
             case 1:
                 assertEquals(expectedCardAmount, game.playerList.get(0).getHandSize());
@@ -190,6 +300,24 @@ public class GameSteps {
                 break;
             case 4:
                 assertEquals(expectedCardAmount, game.playerList.get(3).getHandSize());
+                break;
+        }
+    }
+
+    @And("Player {int} is declared as a Winner")
+    public void player_is_winner(int playerNum) {
+        switch (playerNum) {
+            case 1:
+                assertTrue(output.toString().contains("P1"));
+                break;
+            case 2:
+                assertTrue(output.toString().contains("P2"));
+                break;
+            case 3:
+                assertTrue(output.toString().contains("P3"));
+                break;
+            case 4:
+                assertTrue(output.toString().contains("P4"));
                 break;
         }
     }
--- A1-Nino-Diego-101226510/src/test/resources/CardGame.feature
+++ A1-Nino-Diego-101226510/src/test/resources/CardGame.feature
@@ -2,8 +2,7 @@ Feature: Card Game
 
   Scenario: A1_Scenario
     Given a new game starts with A1_Scenario
-    When A 4 stage quest card
-    And Player 2 sponsors the quest
+    When a "4 Stage Quest" card is drawn with Player 2 sponsoring
     Then Player 1 should have 0 shields
     And Player 3 should have 0 shields
     And Player 4 should have 4 shields
@@ -11,3 +10,11 @@ Feature: Card Game
     And Player 1 should have "F5, value = 5 F10, value = 10 F15, value = 15 F15, value = 15 F30, value = 30 Horse, value = 10 Battle-Axe, value = 15 Battle-Axe, value = 15 Lance, value = 20" in their hand
     And Player 3 should have "F5, value = 5 F5, value = 5 F15, value = 15 F30, value = 30 Sword, value = 10" in their hand
     And Player 4 should have "F15, value = 15 F15, value = 15 F40, value = 40 Lance, value = 20" in their hand
+
+  Scenario: 2winner_game_2winner_quest
+    Given a new game starts with 2winner_game
+    When a "4 Stage Quest" card is drawn with Player 1 sponsoring and a "3 Stage Quest" card is drawn with Player 3 sponsoring
+    Then Player 2 should have 7 shields
+    And Player 4 should have 7 shields
+    And Player 2 is declared as a Winner
+    And Player 4 is declared as a Winner
